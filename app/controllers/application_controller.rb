58242d40aeadc50d2014519c21bc434ef0999f28
flatironschool <t.benson@live.co.uk>
2020-03-03 16:22:00 +0000

    fixed formatting in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::API
 
     def search_recipes
-       request = RecipesApiRequest.new(params[:search_string])
-       results = request.get_recipes
-       render json: results
+        request = RecipesApiRequest.new(params[:search_string])
+        results = request.get_recipes
+        render json: results
     end
 
     def get_recipe
         request = RecipesApiRequest.new(params[:id])
-       results = request.get_recipes
-       render json: results
+        results = request.get_recipes
+        render json: results
     end
 end
